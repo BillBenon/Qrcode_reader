939e3ab1c853f755ce7c7223b4534824b260788d
Dushime Bill Benon <DushimeBillBenon@gmail.com>
2023-11-30 15:49:27 +0200

    init: made it public

---
+++ read_and_save_with_image.py
@@ -0,0 +1,20 @@
+# From line 2 to 11 it deals with creating/generating qrcode in form image
+import qrcode
+img = qrcode.make("https://billbenon.vercel.app/")
+img.save("./Images/Portfolio.jpg")
+
+img = qrcode.make('''
+                  Elected current head of the state.
+                  (The President) - Dushime Bill Benon
+                  ''')
+
+img.save("./Images/President.jpg")
+# End generating Qrcodes in form of image
+
+# From line 15 to 19 it deals with reading qrcode from images and printing them
+import cv2
+
+detect = cv2.QRCodeDetector()
+val, points, straight_qrcode =   detect.detectAndDecode(cv2.imread("./Images/Portfolio.jpg"))
+print(val)
+# End reading Qrcodes in form of image
